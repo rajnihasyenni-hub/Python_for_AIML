18c00ce5bb52be044ac33d7826fa8e1a26e45087
rajnihasyenni-hub <rajnihasyenni@gmail.com>
2026-03-29 23:58:17 +0530

    Add conditional expression for variable assignment

---
+++ Basic_problem_solving/Conditional_expression_conversion.py
@@ -0,0 +1,17 @@
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+x = a + b if (a > 10 and b < 5) else 0
+
+print("Value of x:", x)
+
+
+
+
+#pass or fail
+marks = int(input("Enter marks: "))
+
+if marks > 60:
+    print("Passed")
+else:
+    print("Fail")
